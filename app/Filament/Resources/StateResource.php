6fcf2665d8ca6d29b265503b8dccce58da51f1b1
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2023-12-23 21:23:09 +0530

    feat: Add getRelationship function to StateResource

--- app/Filament/Resources/StateResource.php
+++ app/Filament/Resources/StateResource.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\StateResource\Pages;
 use App\Filament\Resources\StateResource\RelationManagers;
+use App\Filament\Resources\StateResource\RelationManagers\CitiesRelationManager;
+use App\Filament\Resources\StateResource\RelationManagers\EmployeesRelationManager;
 use App\Models\State;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -28,7 +30,7 @@ class StateResource extends Resource
 
     protected static ?string $navigationGroup = 'System Management';
 
-    protected static ?int $navigationSort = 3;
+    protected static ?int $navigationSort = 2;
 
     public static function form(Form $form): Form
     {
@@ -101,7 +103,8 @@ class StateResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            CitiesRelationManager::class,
+            EmployeesRelationManager::class
         ];
     }
 
