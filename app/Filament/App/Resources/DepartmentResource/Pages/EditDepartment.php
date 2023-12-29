5a8b457560d051a9e694faab7cb8a4b5bc56c83f
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2023-12-29 15:50:08 +0530

    feat(app Department Resource): create department Resources files

---
+++ app/Filament/App/Resources/DepartmentResource/Pages/EditDepartment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\App\Resources\DepartmentResource\Pages;
+
+use App\Filament\App\Resources\DepartmentResource;
+use Filament\Actions;
+use Filament\Notifications\Notification;
+use Filament\Resources\Pages\EditRecord;
+
+class EditDepartment extends EditRecord
+{
+    protected static string $resource = DepartmentResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Department updated')
+            ->body('The Department updated successfully.');
+    }
+}
