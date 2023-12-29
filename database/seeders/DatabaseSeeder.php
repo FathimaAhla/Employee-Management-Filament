f65f7e58dba4e843a57ccf631fcc0c383cc84c8c
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2023-12-29 15:53:54 +0530

    feat(database-seeders): add UserSeeder data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Department;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,10 +16,12 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@gmail.com',
+            'password'=> bcrypt('12345678'),
+            'is_admin' => true,
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Admin User',
@@ -29,5 +33,7 @@ class DatabaseSeeder extends Seeder
         $this->call(CountrySeeder::class);
         $this->call(StateSeeder::class);
         $this->call(CitySeeder::class);
+
+        // Department::create(['name' => 'Laravel']);
     }
 }
